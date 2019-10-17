4320c94b0ac4da8257757be703ea1b58a8039dcf
pch18_robo <pch18@qq.com>
2019-10-17 16:51:46 +0900

    加上自动设置的代码和配置

--- build/alpine_x86_64/install.sh
+++ build/alpine_x86_64/install.sh
@@ -1,6 +1,17 @@
-chmod +x ./redsocks
-chmod +x ./redsocksd
+apk update
+apk add libevent
 cp ./redsocks /usr/bin/redsocks
 cp ./redsocks.conf /etc/redsocks.conf
 cp ./redsocksd /etc/init.d/redsocksd
+chmod +x /usr/bin/redsocks
+chmod +x /etc/init.d/redsocksd
 rc-update add redsocksd boot
+
+iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A OUTPUT -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A OUTPUT -d proxy.itc.kansai-u.ac.jp -j RETURN
+iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-ports 12345
+service iptables save
+rc-update add iptables boot
+
+rm -f /etc/profile.d/proxy.sh
